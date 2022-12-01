68626f812c4f4a3ce9edad6754f6a07b9fb18d85
Madhura Bhave <bhavem@vmware.com>
2022-11-30 16:00:54 -0800

    Add component scanning

--- src/main/java/example/helloworld/HelloWorldApplication.java
+++ src/main/java/example/helloworld/HelloWorldApplication.java
@@ -2,8 +2,10 @@ package example.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
