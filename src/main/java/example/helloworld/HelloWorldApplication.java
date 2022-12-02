89ce44cb01073fb63a401380f630a55f5b2f14b3
Madhura Bhave <bhavem@vmware.com>
2022-12-02 13:16:44 -0800

    Replace with SpringBootApplication

--- src/main/java/example/helloworld/HelloWorldApplication.java
+++ src/main/java/example/helloworld/HelloWorldApplication.java
@@ -1,14 +1,11 @@
 package example.helloworld;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@EnableAutoConfiguration
-@ComponentScan
-@Configuration
+@SpringBootApplication
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
