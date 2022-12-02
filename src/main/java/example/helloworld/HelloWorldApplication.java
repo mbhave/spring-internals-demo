6ab0338967f6ea7c1456ce974581efdbf5936bb3
Madhura Bhave <bhavem@vmware.com>
2022-12-02 13:15:54 -0800

    Add configuration

--- src/main/java/example/helloworld/HelloWorldApplication.java
+++ src/main/java/example/helloworld/HelloWorldApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @ComponentScan
+@Configuration
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
